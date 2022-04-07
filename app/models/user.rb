39e0599ac99cab4281090f3bbdb77b4ec5dba7f0
Sagar Chaudhari <sagar.chaudhari@sell.do>
2022-04-07 15:50:14 +0530

    Finish log in/log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  attr_accessor :remember_token
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A([a-z0-9_\-\.]+)@([a-z0-9\-]+\.)([a-z]{2,4}|[0-9]{1,3})\z/i
   validates :email, presence: true, length: {maximum: 255}, 
@@ -7,4 +8,31 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: {minimum: 6}
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+          BCrypt::Engine.cost
+          BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,22 +7,23 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user: { name:"",
-                            email: "user@invalid",
-                            password: "foo",
+      post users_path, user: { name:                   "",
+                            email:         "user@invalid",
+                            password:             "foo",
                             password_confirmation: "bar" }
     end
     assert_template 'users/new'
   end
 
   test "valid signup information" do
-    get signup_patch
+    get signup_path
     assert_difference 'User.count', 1 do
       post_via_redirect users_path, user: { name: "Example",
                                             email: "user@example.com",
-                                            password: "password"
+                                            password: "password",
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
