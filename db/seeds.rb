7e2902f5849e25a58def4ac860d4da7b3b3f980e
Sagar Chaudhari <sagar.chaudhari@sell.do>
2022-04-08 18:56:38 +0530

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create!(name:  "example user",
+            email: "example@rails.org",
+            password: "password",
+            password_confirmation: "password",
+            admin: true)
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@rails.org"
+  password = "password"
+  User.create!(name: name,
+  email: email,
+  password: password,
+  password_confirmation: password)
+end
